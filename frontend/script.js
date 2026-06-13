1a8395c6001e4f6994baefd288d37d3220e98dec
jude <yanksonjude92@gmail.com>
2026-06-13 02:00:41 +0000

    added CORS

--- frontend/script.js
+++ frontend/script.js
@@ -8,7 +8,7 @@ document.getElementById("contactForm")
     message: e.target.message.value
   };
 
-  const res = await fetch("http://127.0.0.1:8000/contact", {
+  const res = await fetch("https://my-portfolio-1-arom.onrender.com/contact", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
